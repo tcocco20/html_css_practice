7ce415397308380fe852593670079ab2be971340
Theo <theococco@gmail.com>
2021-11-18 08:51:16 -0800

    finished adding js to project

--- CSS_Udemy/main_project/shared.js
+++ CSS_Udemy/main_project/shared.js
@@ -2,28 +2,32 @@ const backdrop = document.querySelector(".backdrop");
 const modal = document.querySelector(".modal");
 const selectPlanButtons = document.querySelectorAll(".plan button");
 const modalButtonNegative = document.querySelector(".modal__action--negative");
-const toggleButton = document.querySelector(".toggle-button")
-const mobileNav = document.querySelector(".mobile-nav")
+const toggleButton = document.querySelector(".toggle-button");
+const mobileNav = document.querySelector(".mobile-nav");
 
 for (const button of selectPlanButtons) {
   button.addEventListener("click", () => {
-    backdrop.style.display = "block";
-    modal.style.display = "block";
+    backdrop.classList.add("open");
+    modal.classList.add("open");
   });
 }
 
-modalButtonNegative.addEventListener("click", () => {
-  modal.style.display = "none";
-  backdrop.style.display = "none";
-});
+if (modalButtonNegative) {
+  modalButtonNegative.addEventListener("click", () => {
+    modal.classList.remove("open");
+    backdrop.classList.remove("open");
+  });
+}
 
 backdrop.addEventListener("click", () => {
-  modal.style.display = "none";
-  backdrop.style.display = "none";
-  mobileNav.style.display = "none";
+  if (modal) {
+    modal.classList.remove("open");
+  }
+  backdrop.classList.remove("open");
+  mobileNav.classList.remove("open");
 });
 
 toggleButton.addEventListener("click", () => {
-  mobileNav.style.display = "block";
-  backdrop.style.display = "block";
-})
+  backdrop.classList.add("open");
+  mobileNav.classList.add("open");
+});
