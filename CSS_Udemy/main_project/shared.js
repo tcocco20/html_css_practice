f724ab5417a7d0bdf36143646d395a6eec7e86d5
Theo <theococco@gmail.com>
2021-11-17 20:25:01 -0800

    added hamburger menu

--- CSS_Udemy/main_project/shared.js
+++ CSS_Udemy/main_project/shared.js
@@ -1,1 +1,29 @@
-const backdrop = document.querySelector('.backdrop');
+const backdrop = document.querySelector(".backdrop");
+const modal = document.querySelector(".modal");
+const selectPlanButtons = document.querySelectorAll(".plan button");
+const modalButtonNegative = document.querySelector(".modal__action--negative");
+const toggleButton = document.querySelector(".toggle-button")
+const mobileNav = document.querySelector(".mobile-nav")
+
+for (const button of selectPlanButtons) {
+  button.addEventListener("click", () => {
+    backdrop.style.display = "block";
+    modal.style.display = "block";
+  });
+}
+
+modalButtonNegative.addEventListener("click", () => {
+  modal.style.display = "none";
+  backdrop.style.display = "none";
+});
+
+backdrop.addEventListener("click", () => {
+  modal.style.display = "none";
+  backdrop.style.display = "none";
+  mobileNav.style.display = "none";
+});
+
+toggleButton.addEventListener("click", () => {
+  mobileNav.style.display = "block";
+  backdrop.style.display = "block";
+})
